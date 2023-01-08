5bc39278cd897a6130d4acea0198ef037c2830d7
Chirurgical <denomapple29@gmail.com>
2023-01-08 12:56:43 -0800

    Initial commit

---
+++ Drivebase HW/src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder.Type;
+
+public class Drivebase extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public Drivebase() {
+
+    CANSparkMax m_leftMotor = new CANSparkMAX(0, kBrushless);
+    CANSparkMax m_rightMotor = new CANSparkMAX(1, kBrushless);
+    DifferentialDrive m_drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+  
+    RelativeEncoder m_leftEncoder = m_leftMaster.getAlternateEncoder(Type.kQuadrature,20);
+    RelativeEncoder m_rightEncoder = m_rightMaster.getAlternateEncoder(Type.kQuadrature,20);
+
+    public void arcadeDrive(double forwardOrBackwardSpeed, double turnSpeed){
+      m_drive.arcadeDrive(forwardOrBackwardSpeed, turnSpeed);
+    }
+
+    public void resetEncoders(double xSpeed, double xRotation){
+      m_leftEncoder.setPosition(0);
+      m_rightEncoder.setPosition(0);
+    }
+
+    public double getLeftDistance(){
+      return (m_leftEncoder.getPosition() * distance * Math.PI);
+    }
+
+    public double getRightDistance(){
+      return(m_rightEncoder.getPosition() * distance * Math.PI);
+    }
+    
+    public double getAverageDistance(){
+      return(m_leftEncoder.getPosition() * distance * Math.PI) + (m_rightEncoder.getPosition() * distance * Math.PI) / 2;
+  }
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
